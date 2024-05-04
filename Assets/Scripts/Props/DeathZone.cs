5e62c8c413f6d42ee9412523d70fbbe9caaa2e9f
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-05-04 18:34:54 +0200

    Death zone

--- Assets/Scripts/Props/DeathZone.cs
+++ Assets/Scripts/Props/DeathZone.cs
@@ -1,25 +1,25 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Collections;
-public class LoadSpecificScene : MonoBehaviour
+public class DeathZone : MonoBehaviour
 {
-    public string SceneName;
+    private Transform playerSpawn;
     public Animator fadeSystem;
     private void Awake()
     {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform ;
         fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            StartCoroutine(loadNextScene());
+            StartCoroutine(ReplacePlayer(collision));
         }
     }
-    private IEnumerator loadNextScene()
+    private IEnumerator ReplacePlayer(Collider2D collision)
     {
         fadeSystem.SetTrigger("FadeIn");
         yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene(SceneName);
+        collision.transform.position = playerSpawn.position;
     }
 }
